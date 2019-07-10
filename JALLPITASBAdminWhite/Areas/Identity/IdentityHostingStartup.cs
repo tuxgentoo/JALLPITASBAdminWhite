f6bc3857df7f57808a677aedc81a2aec59feb011
FRANZ WILLAN MARTINEZ CHOQUE <visualsoft.s.a@gmail.com>
2019-07-09 23:27:31 -0400

    Agregar archivos de proyecto.

---
+++ JALLPITASBAdminWhite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JALLPITASBAdminWhite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(JALLPITASBAdminWhite.Areas.Identity.IdentityHostingStartup))]
+namespace JALLPITASBAdminWhite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
